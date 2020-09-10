006d84e807dfcf9bebdbab3605854029140a3c51
vagrant <vagrant@localhost.localdomain>
2020-09-10 04:25:20 +0000

    initial

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'home#top'
+  resources :books do
+  	resources :book_comments, only: [:create, :destroy]
+  	resource :favorites, only: [:create, :destroy]
+  end
+  resources :users do
+  	member do
+     get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
+  get 'home/about' => 'home#about'
+  get 'search' => 'searchs#search'
+end
